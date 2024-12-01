7493ea9e27d0ccd64da6cf429236675a18dac65d
christysharankumari <christinasharanyaa11a@gmail.com>
2024-12-01 12:02:06 +0530

    requiremets new

--- app.py
+++ app.py
@@ -245,5 +245,5 @@ def delete_account():
 
     return render_template('delete_account.html',form=form)
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
